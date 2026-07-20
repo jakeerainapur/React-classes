311873dddedb428f588f93f932dd6566c73bc8d0
jakeer-ainapur <jakeerainapur786@gmail.com>
2026-07-20 20:01:50 +0530

    files added

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import {createRoot} from "react-dom/client"
+import "./index.css"
+
+
+ createRoot(document.getElementById("root")).render(
+   <div className="card">
+    
+     <nav>
+      <img className="logo" 
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBj0m1wajVSVYdMAq36XcDCyl-ioKTkW-GqG2V2ZiDTNdhzzh1YcOAwolH&s=10" alt=" Logo not found" />
+        <h1 className="rule">Rules of JSX</h1>
+     </nav>
+     <ul className="tables">
+      <li>Must enclosed JSX in a Root Element</li>
+      <li> Choose the tags Property</li>
+      <li> use className for selector not for class</li>
+      <li> Use camelcase for attributes</li>
+      <li> use flower braces for js</li>
+     </ul>   
+   </div>
+ 
+ );
